840e2e707785a3cda376d76d89436691604448f0
Mike Jackson <mjackson@touchcommerce.com>
2018-03-12 18:54:13 -0700

    adding missing files

---
+++ ui/src/app/key-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import {KeyPipePipe} from './key-pipe.pipe';
+
+describe('KeyPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new KeyPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
